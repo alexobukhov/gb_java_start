d843e64aad2869b3d3ea25abbaf3598a3ba4c2e4
am.obukhov <Kate17032021>
2021-12-16 16:19:42 +0300

    Homework 6 with * tasks

---
+++ src/ru/gb/lesson6/lesson6.java
@@ -0,0 +1,42 @@
+package ru.gb.lesson6;
+
+public class lesson6 {
+    public static void main(String[] args) {
+        Dog dogSharik = new Dog("Шарик");
+        Cat catMurzik = new Cat("Мурзик");
+        Dog dogTuzik = new Dog("Тузик");
+        Cat catPolosatik = new Cat("Полосатик");
+
+        dogSharik.run(600);
+        catMurzik.run(300);
+        dogTuzik.run(400);
+        catMurzik.run(130);
+        dogSharik.swim(15);
+        dogTuzik.swim(5);
+        catMurzik.swim(7);
+
+        Animal[] animals = {
+                dogSharik,
+                catMurzik,
+                dogTuzik,
+                catPolosatik
+        };
+
+        int countAnimals = 0;
+        int countDogs = 0;
+        int countCats = 0;
+        for (Animal animal: animals) {
+            if (animal instanceof Animal) {
+                countAnimals++;
+            };
+            if (animal instanceof Dog) {
+                countDogs++;
+            };
+            if (animal instanceof Cat) {
+                countCats++;
+            }
+        }
+        System.out.println("Всего: собак - " + countDogs + ", котов - " + countCats + ", животных - "
+                + countAnimals + ".");
+    }
+}
