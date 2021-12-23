1ce2368049c918e31b555846847703359b1bf266
am.obukhov <Kate17032021>
2021-12-23 10:13:20 +0300

    Homework 7 with all tasks

---
+++ src/ru/gb/lesson7/oop/Plate.java
@@ -0,0 +1,37 @@
+package ru.gb.lesson7.oop;
+
+public class Plate {
+
+    private int foodCount;
+
+    public Plate(int foodCount) {
+        this.foodCount = foodCount;
+    }
+
+    public void printInfo() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Plate{" + "foodCount=" + foodCount + '}';
+    }
+
+    public Boolean decreaseFood(int catEatFoodCount) {
+        Boolean catSatiety = foodCount >= catEatFoodCount;
+        foodCount -= catSatiety ? catEatFoodCount : 0;
+        return catSatiety;
+    }
+
+    public int getFoodCount() {
+        return foodCount;
+    }
+
+    public void setFoodCount(int foodCount) {
+        this.foodCount = foodCount;
+    }
+
+    public void addFood(int addFood) {
+        foodCount += addFood;
+    }
+}
